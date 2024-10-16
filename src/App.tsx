873b03e14a1c1a9559443e90b8df2045af36ca51
ec50n9 <shelloworld@qq.com>
2024-10-16 10:02:30 +0800

    feat: 添加路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,130 +1,21 @@
 import "./App.css";
-import { Button } from "@/components/ui/button";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import {
-  LuSquirrel,
-  LuSettings,
-  LuLassoSelect,
-  LuClipboardList,
-  LuAlarmClock,
-  LuAward,
-  LuHome,
-  LuBellRing,
-} from "react-icons/lu";
-import { cn } from "./lib/utils";
 import { DatabaseProvider } from "@/contexts/DatabaseContext";
-import { useDatabase } from "@/hooks/useDatabaseHook";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import DefaultLayout from "@/layouts/DefaultLayout";
+import HomeView from "@/views/Home";
+import TestView from "@/views/Test";
 
 export default function App() {
   return (
     <DatabaseProvider>
-      <Layout />
+      <Router>
+        <DefaultLayout>
+          <Routes>
+            <Route path="/" element={<HomeView />} />
+            <Route path="/test" element={<TestView />} />
+          </Routes>
+        </DefaultLayout>
+      </Router>
     </DatabaseProvider>
-  );
-}
-
-function Layout() {
-  const { db } = useDatabase();
-  const { db: db2 } = useDatabase();
-  console.log("get db:", db);
-  console.log("get db2:", db2);
-  console.log("is equal:", db === db2);
-
-  const handleInsertTodo = async () => {
-    if (!db) return;
-    const result = await db.execute(
-      "INSERT into todos (id, title, status) VALUES ($1, $2, $3)",
-      [1, "hello, todo", "done"],
-    );
-    console.log("insert result:", result);
-  };
-
-  return (
-    <div className="h-screen grid grid-cols-[auto_1fr_auto] gap-8 bg-[#faf7f5] text-slate-700 p-4">
-      <SideBar className="rounded-xl" />
-
-      <div className="of-auto">
-        <h1 className="mt-5 text-4xl font-medium leading-snug">
-          下午好，
-          <br />
-          今天好好学习了没？
-        </h1>
-        <div>
-          <Button onClick={handleInsertTodo}>添加任务</Button>
-        </div>
-      </div>
-
-      <UserInfoPanel className="w-72 rounded-3xl" />
-    </div>
-  );
-}
-
-function SideBar(props: { className?: string }) {
-  return (
-    <div
-      className={cn(
-        "flex flex-col justify-between items-center gap-8 bg-[#f1e8e1] p-4",
-        props.className,
-      )}
-    >
-      <div className="flex flex-col items-center gap-4">
-        <LuSquirrel className="text-3xl" />
-        <Button variant="default" size="icon" className="rounded-full">
-          <LuHome className="size-4" />
-        </Button>
-      </div>
-
-      <div className="flex flex-col gap-2">
-        <Button variant="outline" size="icon" className="rounded-full">
-          <LuLassoSelect className="size-4" />
-        </Button>
-        <Button variant="outline" size="icon" className="rounded-full">
-          <LuClipboardList className="size-4" />
-        </Button>
-        <Button variant="outline" size="icon" className="rounded-full">
-          <LuAlarmClock className="size-4" />
-        </Button>
-        <Button variant="outline" size="icon" className="rounded-full">
-          <LuAward className="size-4" />
-        </Button>
-      </div>
-
-      <div className="flex flex-col gap-2">
-        <Button variant="outline" size="icon" className="rounded-full">
-          <LuSettings className="size-4" />
-        </Button>
-        <Avatar className="size-8">
-          <AvatarImage src="https://github.com/shadcn.png" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
-      </div>
-    </div>
-  );
-}
-
-function UserInfoPanel(props: { className?: string }) {
-  return (
-    <div
-      className={cn(
-        "flex flex-col bg-[#f1e8e1] p-4 overflow-auto",
-        props.className,
-      )}
-    >
-      <div className="flex justify-between">
-        <LuBellRing className="size-5" />
-        <div className="flex flex-col items-center pt-3">
-          <Avatar className="size-14">
-            <AvatarImage src="https://github.com/shadcn.png" />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-          <p className="mt-2 text-xl font-medium">梁高菘</p>
-        </div>
-        <LuSettings className="size-5" />
-      </div>
-
-      <div className="mt-4 bg-white px-3 py-3 rounded-full">
-        <LuSettings />
-      </div>
-    </div>
   );
 }
---
+++ src/layouts/DefaultLayout.tsx
@@ -0,0 +1,131 @@
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  LuSquirrel,
+  LuSettings,
+  LuLassoSelect,
+  LuClipboardList,
+  LuAlarmClock,
+  LuAward,
+  LuHome,
+  LuBellRing,
+} from "react-icons/lu";
+import { useLocation, Link } from "react-router-dom";
+import { cn } from "@/lib/utils";
+
+export default function DefaultLayout(props: { children: React.ReactNode }) {
+  return (
+    <div className="h-screen grid grid-cols-[auto_1fr_auto] gap-8 bg-[#faf7f5] text-slate-700 p-4">
+      <SideBar className="rounded-xl" />
+
+      <div className="of-auto">
+        <h1 className="mt-5 text-4xl font-medium leading-snug">
+          下午好，
+          <br />
+          今天好好学习了没？
+        </h1>
+        <div>{props.children}</div>
+      </div>
+
+      <UserInfoPanel className="w-72 rounded-3xl" />
+    </div>
+  );
+}
+
+function SideBar(props: { className?: string }) {
+  const routes = {
+    top: [{ name: "首页", icon: LuHome, path: "/" }],
+    center: [
+      { name: "点名", icon: LuLassoSelect, path: "/test" },
+      { name: "积分", icon: LuClipboardList, path: "/test" },
+      { name: "计时器", icon: LuAlarmClock, path: "/test" },
+      { name: "排行榜", icon: LuAward, path: "/test" },
+    ],
+    bottom: [{ name: "设置", icon: LuSettings, path: "/test" }],
+  };
+
+  const location = useLocation();
+
+  return (
+    <div
+      className={cn(
+        "flex flex-col justify-between items-center gap-8 bg-[#f1e8e1] p-4",
+        props.className,
+      )}
+    >
+      <div className="flex flex-col items-center gap-4">
+        <LuSquirrel className="text-3xl" />
+        {routes.top.map((item) => (
+          <Link key={item.name} to={item.path}>
+            <Button
+              variant={location.pathname === item.path ? "default" : "outline"}
+              size="icon"
+              className="rounded-full"
+            >
+              <item.icon className="size-4" />
+            </Button>
+          </Link>
+        ))}
+      </div>
+
+      <div className="flex flex-col gap-2">
+        {routes.center.map((item) => (
+          <Link key={item.name} to={item.path}>
+            <Button
+              variant={location.pathname === item.path ? "default" : "outline"}
+              size="icon"
+              className="rounded-full"
+            >
+              <item.icon className="size-4" />
+            </Button>
+          </Link>
+        ))}
+      </div>
+
+      <div className="flex flex-col gap-2">
+        {routes.bottom.map((item) => (
+          <Link key={item.name} to={item.path}>
+            <Button
+              variant={location.pathname === item.path ? "default" : "outline"}
+              size="icon"
+              className="rounded-full"
+            >
+              <item.icon className="size-4" />
+            </Button>
+          </Link>
+        ))}
+        <Avatar className="size-8">
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </div>
+    </div>
+  );
+}
+
+function UserInfoPanel(props: { className?: string }) {
+  return (
+    <div
+      className={cn(
+        "flex flex-col bg-[#f1e8e1] p-4 overflow-auto",
+        props.className,
+      )}
+    >
+      <div className="flex justify-between">
+        <LuBellRing className="size-5" />
+        <div className="flex flex-col items-center pt-3">
+          <Avatar className="size-14">
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          <p className="mt-2 text-xl font-medium">梁高菘</p>
+        </div>
+        <LuSettings className="size-5" />
+      </div>
+
+      <div className="mt-4 bg-white px-3 py-3 rounded-full">
+        <LuSettings />
+      </div>
+    </div>
+  );
+}
