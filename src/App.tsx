3f3210b6753caacb662afc47b0d7a01d03de507c
ec50n9 <shelloworld@qq.com>
2024-10-23 19:08:18 +0800

    chore: 去除多余代码

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import "./App.css";
-import { DatabaseProvider } from "@/providers/database-provider";
 import { ThemeProvider } from "@/providers/theme-provider";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { RouterProvider } from "react-router-dom";
@@ -7,12 +6,10 @@ import router from "./routes/router";
 
 export default function App() {
   return (
-    <DatabaseProvider>
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <TooltipProvider>
-          <RouterProvider router={router} />
-        </TooltipProvider>
-      </ThemeProvider>
-    </DatabaseProvider>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <TooltipProvider>
+        <RouterProvider router={router} />
+      </TooltipProvider>
+    </ThemeProvider>
   );
 }
