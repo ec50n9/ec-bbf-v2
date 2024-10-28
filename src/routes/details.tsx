2157707751a892c317d97ca5187d1fb5c2ed5db0
ec50n9 <shelloworld@qq.com>
2024-10-28 19:47:31 +0800

    feat: 优化详情页ui

--- src/routes/details.tsx
+++ src/routes/details.tsx
@@ -1,3 +1,4 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { getStudent } from "@/services/student";
 import { Student } from "@/services/types";
 import { usePluginStore } from "@/stores/plugin-store";
@@ -9,7 +10,7 @@ export default function Details() {
   const [student, setStudent] = useState<Student>();
 
   const allInfoViews = usePluginStore((s) => s.infoViews);
-  console.log('all info views:', allInfoViews)
+  console.log("all info views:", allInfoViews);
   const infoViews = useMemo(
     () => allInfoViews.filter((i) => i.supportedTypes === Student),
     [allInfoViews],
@@ -19,9 +20,10 @@ export default function Details() {
     (async () => {
       if (type === "student") {
         const [_student] = await getStudent(Number(id));
-        console.log("学生:", student);
         if (!_student) throw new Error("学生不存在");
         setStudent(_student);
+
+        console.log("学生:", student);
       }
     })();
 
@@ -31,8 +33,17 @@ export default function Details() {
   return (
     student && (
       <div className="h-screen overflow-auto">
-        <h1 className="mt-5 mx-4 text-2xl font-medium">{student.name}</h1>
-        {infoViews.map((view) => view.component({ data: student }))}
+        <div className="mt-5 flex flex-col items-center gap-1">
+          <Avatar className="size-20">
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>{student.name.slice(-2)}</AvatarFallback>
+          </Avatar>
+          <h1 className="text-lg font-medium">{student.name}</h1>
+        </div>
+
+        {infoViews.map((view) => (
+          <view.component key={view.component.name} data={student} />
+        ))}
       </div>
     )
   );
